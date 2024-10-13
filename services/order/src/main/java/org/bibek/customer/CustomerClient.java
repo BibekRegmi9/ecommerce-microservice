731fe4ef288800f7467b89bfbc12ba8f5a6a723f
Bibek Regmi <vivekregmi83@gmail.com>
2024-10-13 17:56:37 +0545

    fix: kafka consumer not working

--- services/order/src/main/java/org/bibek/customer/CustomerClient.java
+++ services/order/src/main/java/org/bibek/customer/CustomerClient.java
@@ -11,6 +11,7 @@ import java.util.Optional;
         name = "customer-service",
         url = "${application.config.customer-url}"
 )
+
 public interface CustomerClient {
 
     @GetMapping("/{customer-id}")
