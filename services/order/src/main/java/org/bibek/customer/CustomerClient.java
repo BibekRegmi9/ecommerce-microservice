e43bddca07ff8f84642857bf5a4c77ba88e9891a
bibekregmi007 <bibek.regmi@yenyasoft.com>
2024-10-16 12:02:39 +0545

    fix: confuguration fix in order service

--- services/order/src/main/java/org/bibek/customer/CustomerClient.java
+++ services/order/src/main/java/org/bibek/customer/CustomerClient.java
@@ -1,6 +1,7 @@
 package org.bibek.customer;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
